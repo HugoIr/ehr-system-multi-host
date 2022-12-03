32e10bebce8a646c74af7d5e65d3af2f954138df
“HugoIr” <hugoirwanto23@gmail.com>
2022-12-03 15:49:25 +0700

    update

--- channel-up.sh
+++ channel-up.sh
@@ -1,7 +1,7 @@
 sudo docker exec cli peer channel create -o orderer.example.com:7050 -c hospital-channel -f ./channel-artifacts/hospital-channel.tx --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/consortium/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
 sleep 5
 
-sudo docker exec cli peer channel join -b ./channel-artifacts/hospital-channel.tx
+sudo docker exec cli peer channel join -b ./channel-artifacts/hospital-channel.block
 
 sudo docker exec -e CORE_PEER_ADDRESS=peer1.hospital:8051 -e CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/consortium/crypto-config/peerOrganizations/hospital/peers/peer1.hospital/tls/ca.crt cli peer channel join -b ./channel-artifacts/hospital-channel.block
 
