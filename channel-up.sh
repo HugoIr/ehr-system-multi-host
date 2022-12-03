c48c5c83cf3f47cef813958f1e35912e30a89100
“HugoIr” <hugoirwanto23@gmail.com>
2022-12-03 22:30:32 +0700

    ip

--- channel-up.sh
+++ channel-up.sh
@@ -5,7 +5,7 @@ sleep 5
 
 sudo docker exec cli peer channel join -b ./channel-artifacts/hospital-channel.block
 
-sudo docker exec -e CORE_PEER_TLS_ENABLED=true -e CORE_PEER_LOCALMSPID="HospitalMSP" -e CORE_PEER_ADDRESS=34.128.89.87:8051 -e CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/consortium/crypto-config/peerOrganizations/hospital/users/Admin@hospital/msp -e CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/consortium/crypto-config/peerOrganizations/hospital/peers/peer1.hospital/tls/ca.crt cli peer channel join -b ./channel-artifacts/hospital-channel.block
+sudo docker exec -e CORE_PEER_TLS_ENABLED=true -e CORE_PEER_LOCALMSPID="HospitalMSP" -e CORE_PEER_ADDRESS=peer1.hospital:8051 -e CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/consortium/crypto-config/peerOrganizations/hospital/users/Admin@hospital/msp -e CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/consortium/crypto-config/peerOrganizations/hospital/peers/peer1.hospital/tls/ca.crt cli peer channel join -b ./channel-artifacts/hospital-channel.block
 
 sudo docker exec -e CORE_PEER_TLS_ENABLED=true -e CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/consortium/crypto-config/peerOrganizations/insurance/users/Admin@insurance/msp -e CORE_PEER_ADDRESS=34.101.138.254:9051 -e CORE_PEER_LOCALMSPID="InsuranceMSP" -e CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/consortium/crypto-config/peerOrganizations/insurance/peers/peer0.insurance/tls/ca.crt cli peer channel join -b ./channel-artifacts/hospital-channel.block
 
