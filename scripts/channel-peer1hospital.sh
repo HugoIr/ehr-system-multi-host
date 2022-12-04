ab78880965e15f9adf5144047fdca9057cdac84f
“HugoIr” <hugoirwanto23@gmail.com>
2022-12-04 23:16:11 +0700

    tmp command joinchannel

--- scripts/channel-peer1hospital.sh
+++ scripts/channel-peer1hospital.sh
@@ -8,8 +8,8 @@
 FABRIC_CFG_PATH=$PWD/config
 BLOCKFILE="./channel-artifacts/${CHANNEL_NAME}.block"
 
-infoln "Joining org1 peer1.hospital1 to the channel..."
-joinChannel 3
+# infoln "Joining org1 peer1.hospital1 to the channel..."
+# joinChannel 3
 infoln "Setting anchor peer1.hospital for org2..."
 setAnchorPeer 3
 
