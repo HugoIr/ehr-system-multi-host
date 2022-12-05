2bf2d5c7bbfdf60407ad476f46f0f531afa73ab7
“HugoIr” <hugoirwanto23@gmail.com>
2022-12-05 21:40:28 +0700

    refactor

--- client-api/api/utils/converter.js
+++ client-api/api/utils/converter.js
@@ -1,13 +1,10 @@
 const EHRParser = (obj) => {
-    console.log("OBJH ", obj)
-    console.log("obj[0]['Record'] ", obj[0]['Record'])
     obj[0]['Record']["medicalHistory"]= JSON.parse(obj[0]['Record']["medicalHistory"]);
     return obj;
 }
 
 const getAllEhrParser = (stringEhr) => {
     let result = JSON.parse(stringEhr)
-    console.log("result ", result[0])
     result.map((item, index) => {
         result[index]['Record']["medicalHistory"] = JSON.parse(item['Record']['medicalHistory'])
         result[index]['Record']["diagnose"] = JSON.parse(item['Record']['diagnose'])
@@ -26,7 +23,6 @@ const getEhrParser = (stringEhr) => {
 
 const getHistoryEhrParser = (stringEhr) => {
     let result = JSON.parse(stringEhr)
-    console.log("result ", result[0])
     result.map((item, index) => {
         result[index]['value']["medicalHistory"] = JSON.parse(item['value']['medicalHistory'])
         result[index]['value']["diagnose"] = JSON.parse(item['value']['diagnose'])
