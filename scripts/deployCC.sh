2ce55d64b6678e1b36aed02b2890e2ca824c38a6
“HugoIr” <hugoirwanto23@gmail.com>
2022-12-08 15:51:40 +0700

    changes

--- scripts/deployCC.sh
+++ scripts/deployCC.sh
@@ -282,24 +282,24 @@ chaincodeQuery() {
   fi
 }
 
-# # package the chaincode
-# packageChaincode
+# package the chaincode
+packageChaincode
 
-# ## Install chaincode on peer0.hospital and peer0.org2
-# infoln "Installing chaincode on peer0.hospital..."
-# installChaincode 1
+## Install chaincode on peer0.hospital and peer0.org2
+infoln "Installing chaincode on peer0.hospital..."
+installChaincode 1
 
-# infoln "Install chaincode on peer1.hospital..."
-# installChaincode 3
+infoln "Install chaincode on peer1.hospital..."
+installChaincode 3
 
-# infoln "Install chaincode on peer0.insurance..."
-# installChaincode 2
+infoln "Install chaincode on peer0.insurance..."
+installChaincode 2
 
 # query whether the chaincode is installed
 queryInstalled 1
 
 # approve the definition for hospital
-# approveForMyOrg 1
+approveForMyOrg 1
 
 ## check whether the chaincode definition is ready to be committed
 ## expect hospital to have approved and org2 not to
