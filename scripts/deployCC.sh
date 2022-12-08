e799ad845b812d1127645ea7c3baa286ac957645
“HugoIr” <hugoirwanto23@gmail.com>
2022-12-08 15:42:02 +0700

    change

--- scripts/deployCC.sh
+++ scripts/deployCC.sh
@@ -303,8 +303,8 @@ queryInstalled 1
 
 ## check whether the chaincode definition is ready to be committed
 ## expect hospital to have approved and org2 not to
-checkCommitReadiness 1 "\"HospitalMSP\": true" "\"InsuranceMSP\": false"
-checkCommitReadiness 2 "\"HospitalMSP\": true" "\"InsuranceMSP\": false"
+checkCommitReadiness 1 "\"HospitalMSP\": true" "\"InsuranceMSP\": true"
+checkCommitReadiness 2 "\"HospitalMSP\": true" "\"InsuranceMSP\": true"
 
 ## now approve also for insurance
 approveForMyOrg 2
