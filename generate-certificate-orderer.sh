a94b04068760af02bb0e2cd5e19c6bfa629b2e9a
“HugoIr” <hugoirwanto23@gmail.com>
2022-12-14 19:04:08 +0700

    add 2 new orderer

--- generate-certificate-orderer.sh
+++ generate-certificate-orderer.sh
@@ -38,10 +38,28 @@ function generateOrderer() {
   fabric-ca-client register --caname ca-orderer --id.name ordererAdmin --id.secret ordererAdminpw --id.type admin --tls.certfiles ${PWD}/consortium/fabric-ca/ordererOrg/tls-cert.pem
   { set +x; } 2>/dev/null
 
+
+  infoln "Registering orderer 2"
+  set -x
+  fabric-ca-client register --caname ca-orderer --id.name orderer2 --id.secret ordererpw --id.type orderer --tls.certfiles ${PWD}/consortium/fabric-ca/ordererOrg/tls-cert.pem
+  { set +x; } 2>/dev/null
+
+
+  infoln "Registering orderer 3"
+  set -x
+  fabric-ca-client register --caname ca-orderer --id.name orderer3 --id.secret ordererpw --id.type orderer --tls.certfiles ${PWD}/consortium/fabric-ca/ordererOrg/tls-cert.pem
+  { set +x; } 2>/dev/null
+  # infoln "Registering the orderer 2 admin"
+  # set -x
+  # fabric-ca-client register --caname ca-orderer --id.name ordererAdmin2 --id.secret ordererAdminpw --id.type admin --tls.certfiles ${PWD}/consortium/fabric-ca/ordererOrg/tls-cert.pem
+  # { set +x; } 2>/dev/null
+
   mkdir -p consortium/crypto-config/ordererOrganizations/example.com/orderers
   mkdir -p consortium/crypto-config/ordererOrganizations/example.com/orderers/example.com
 
   mkdir -p consortium/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com
+  mkdir -p consortium/crypto-config/ordererOrganizations/example.com/orderers/orderer2.example.com
+  mkdir -p consortium/crypto-config/ordererOrganizations/example.com/orderers/orderer3.example.com
 
   infoln "Generating the orderer msp"
   set -x
@@ -70,7 +88,59 @@ function generateOrderer() {
   fabric-ca-client enroll -u https://ordererAdmin:ordererAdminpw@34.101.204.172:9054 --caname ca-orderer -M ${PWD}/consortium/crypto-config/ordererOrganizations/example.com/users/Admin@example.com/msp --tls.certfiles ${PWD}/consortium/fabric-ca/ordererOrg/tls-cert.pem
   { set +x; } 2>/dev/null
 
+
   cp ${PWD}/consortium/crypto-config/ordererOrganizations/example.com/msp/config.yaml ${PWD}/consortium/crypto-config/ordererOrganizations/example.com/users/Admin@example.com/msp/config.yaml
+
+
+
+  infoln "Generating the orderer 2 msp"
+  set -x
+  fabric-ca-client enroll -u https://orderer2:ordererpw@34.101.204.172:9054 --caname ca-orderer -M ${PWD}/consortium/crypto-config/ordererOrganizations/example.com/orderers/orderer2.example.com/msp --csr.hosts orderer2.example.com --csr.hosts 34.128.89.87 --tls.certfiles ${PWD}/consortium/fabric-ca/ordererOrg/tls-cert.pem
+  { set +x; } 2>/dev/null
+
+  cp ${PWD}/consortium/crypto-config/ordererOrganizations/example.com/msp/config.yaml ${PWD}/consortium/crypto-config/ordererOrganizations/example.com/orderers/orderer2.example.com/msp/config.yaml
+
+  infoln "Generating the orderer-tls 2 certificates"
+  set -x
+  fabric-ca-client enroll -u https://orderer2:ordererpw@34.101.204.172:9054 --caname ca-orderer -M ${PWD}/consortium/crypto-config/ordererOrganizations/example.com/orderers/orderer2.example.com/tls --enrollment.profile tls  --csr.hosts orderer2.example.com --csr.hosts 34.128.89.87 --tls.certfiles ${PWD}/consortium/fabric-ca/ordererOrg/tls-cert.pem
+  { set +x; } 2>/dev/null
+
+  cp ${PWD}/consortium/crypto-config/ordererOrganizations/example.com/orderers/orderer2.example.com/tls/tlscacerts/* ${PWD}/consortium/crypto-config/ordererOrganizations/example.com/orderers/orderer2.example.com/tls/ca.crt
+  cp ${PWD}/consortium/crypto-config/ordererOrganizations/example.com/orderers/orderer2.example.com/tls/signcerts/* ${PWD}/consortium/crypto-config/ordererOrganizations/example.com/orderers/orderer2.example.com/tls/server.crt
+  cp ${PWD}/consortium/crypto-config/ordererOrganizations/example.com/orderers/orderer2.example.com/tls/keystore/* ${PWD}/consortium/crypto-config/ordererOrganizations/example.com/orderers/orderer2.example.com/tls/server.key
+
+  mkdir -p ${PWD}/consortium/crypto-config/ordererOrganizations/example.com/orderers/orderer2.example.com/msp/tlscacerts
+  cp ${PWD}/consortium/crypto-config/ordererOrganizations/example.com/orderers/orderer2.example.com/tls/tlscacerts/* ${PWD}/consortium/crypto-config/ordererOrganizations/example.com/orderers/orderer2.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+
+  mkdir -p ${PWD}/consortium/crypto-config/ordererOrganizations/example.com/msp/tlscacerts
+  cp ${PWD}/consortium/crypto-config/ordererOrganizations/example.com/orderers/orderer2.example.com/tls/tlscacerts/* ${PWD}/consortium/crypto-config/ordererOrganizations/example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+
+
+  infoln "Generating the orderer 3 msp"
+  set -x
+  fabric-ca-client enroll -u https://orderer3:ordererpw@34.101.204.172:9054 --caname ca-orderer -M ${PWD}/consortium/crypto-config/ordererOrganizations/example.com/orderers/orderer3.example.com/msp --csr.hosts orderer3.example.com --csr.hosts 34.101.138.254 --tls.certfiles ${PWD}/consortium/fabric-ca/ordererOrg/tls-cert.pem
+  { set +x; } 2>/dev/null
+
+  cp ${PWD}/consortium/crypto-config/ordererOrganizations/example.com/msp/config.yaml ${PWD}/consortium/crypto-config/ordererOrganizations/example.com/orderers/orderer3.example.com/msp/config.yaml
+
+  infoln "Generating the orderer-tls 3 certificates"
+  set -x
+  fabric-ca-client enroll -u https://orderer3:ordererpw@34.101.204.172:9054 --caname ca-orderer -M ${PWD}/consortium/crypto-config/ordererOrganizations/example.com/orderers/orderer3.example.com/tls --enrollment.profile tls  --csr.hosts orderer3.example.com --csr.hosts 34.101.138.254 --tls.certfiles ${PWD}/consortium/fabric-ca/ordererOrg/tls-cert.pem
+  { set +x; } 2>/dev/null
+
+  cp ${PWD}/consortium/crypto-config/ordererOrganizations/example.com/orderers/orderer3.example.com/tls/tlscacerts/* ${PWD}/consortium/crypto-config/ordererOrganizations/example.com/orderers/orderer3.example.com/tls/ca.crt
+  cp ${PWD}/consortium/crypto-config/ordererOrganizations/example.com/orderers/orderer3.example.com/tls/signcerts/* ${PWD}/consortium/crypto-config/ordererOrganizations/example.com/orderers/orderer3.example.com/tls/server.crt
+  cp ${PWD}/consortium/crypto-config/ordererOrganizations/example.com/orderers/orderer3.example.com/tls/keystore/* ${PWD}/consortium/crypto-config/ordererOrganizations/example.com/orderers/orderer3.example.com/tls/server.key
+
+  mkdir -p ${PWD}/consortium/crypto-config/ordererOrganizations/example.com/orderers/orderer3.example.com/msp/tlscacerts
+  cp ${PWD}/consortium/crypto-config/ordererOrganizations/example.com/orderers/orderer3.example.com/tls/tlscacerts/* ${PWD}/consortium/crypto-config/ordererOrganizations/example.com/orderers/orderer3.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+
+  mkdir -p ${PWD}/consortium/crypto-config/ordererOrganizations/example.com/msp/tlscacerts
+  cp ${PWD}/consortium/crypto-config/ordererOrganizations/example.com/orderers/orderer3.example.com/tls/tlscacerts/* ${PWD}/consortium/crypto-config/ordererOrganizations/example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+
+
 }
+
+
 
 generateOrderer
